9fe053082eee39258cc03d929b4a4dd11259400e
Joao Rocha <joaorocha@joaos-mac.local>
2018-05-01 00:18:21 -0300

    Initial Commit

---
+++ IMC/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  IMC
+//
+//  Created by Joao Rocha on 30/04/18.
+//  Copyright © 2018 Joao Rocha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tfWeight: UITextField!
+    @IBOutlet weak var tfHeight: UITextField!
+    @IBOutlet weak var lbResult: UILabel!
+    @IBOutlet weak var ivResult: UIImageView!
+    @IBOutlet weak var viewResult: UILabel!
+    @IBOutlet weak var viResult: UIView!
+    
+    var imc: Double = 0;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculate(_ sender: Any) {
+        if let w = Double(tfWeight.text!), let h = Double(tfHeight.text!) {
+            imc = w / (h * h)
+            showResults()
+        }
+    }
+    
+    func showResults() {
+        var result: String = ""
+        var image: String = ""
+        
+        switch imc {
+            case 0..<16:
+                result = "Magreza"
+                image = "abaixo"
+            case 16..<18.5:
+                result = "Abaixo do peso"
+                image = "abaixo"
+            case 18.5..<25:
+                result = "Peso Ideal"
+                image = "ideal"
+            case 25..<30:
+                result = "Sobrepeso"
+                image = "sobre"
+            default:
+                result = "Obesidade"
+                image = "obesidade"
+        }
+        lbResult.text = "\(Int(imc)): \(result)";
+        ivResult.image = UIImage(named: image)
+        viResult.isHidden = false;
+        view.endEditing(true)
+    }
+    
+}
+
